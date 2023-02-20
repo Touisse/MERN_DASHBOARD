b9df2100809b597bfb896f5da0fb34638f46e75c
Touisse <yassin.touisd@gmail.com>
2023-02-20 15:03:34 +0100

    the proprty filtering and sorting done in the backend

--- server/controllers/property.controller.js
+++ server/controllers/property.controller.js
@@ -12,8 +12,36 @@ cloudinary.config({
 });
 
 const getAllProperties = async (req, res) => {
+  const {
+    _end,
+    _order,
+    _start,
+    _sort,
+    title_like = "",
+    propertyType = "",
+  } = req.query;
+
+  const query = {};
+
+  if (propertyType !== "") {
+    query.propertyType = propertyType;
+  }
+
+  if (title_like) {
+    query.title = { $regex: title_like, $options: "i" };
+  }
+
   try {
-    const properties = await Property.find({}).limit(req.query._end);
+    const count = await Property.countDocuments({ query });
+
+    const properties = await Property.find(query)
+      .limit(_end)
+      .skip(_start)
+      .sort({ [_sort]: _order });
+
+    res.header("x-total-count", count);
+    res.header("Access-Control-Expose-Headers", "x-total-count");
+
     res.status(200).json(properties);
   } catch (error) {
     res.status(500).json({ message: error.message });
